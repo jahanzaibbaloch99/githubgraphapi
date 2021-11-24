0083ffe16dd89b0ba58f5775b4258be2fb3c3768
Jahanzaib Ali <jhanzeb.ali99@gmail.com>
2021-11-24 15:55:09 +0500

    client auth fix

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import {
   InMemoryCache,
   ApolloProvider,
   HttpLink,
+  createHttpLink,
 } from "@apollo/client";
 import Home from "./Screens/Home/Home";
+import { setContext } from "@apollo/client/link/context";
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "https://api.github.com/graphql",
-    credentials: "same-origin",
-    headers: {
-      Authorization: `Bearer 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`,
-    },
-  }),
-  cache: new InMemoryCache(),
-});
-console.log(client, "");
 function App() {
+  const httpLink = createHttpLink({
+    uri: "https://api.github.com/graphql",
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = "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";
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
   return (
     <ApolloProvider client={client}>
       <Home />
